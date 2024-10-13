5e84f5cbc4113e543040b69a9c96326d8c30c4a7
Henrique Mendes <henriquemendes13@gmail.com>
2024-10-13 14:05:39 -0300

    Config gitignore

--- FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcca1c4739ec4ff0a7e9131f39a8c602f1c520b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
