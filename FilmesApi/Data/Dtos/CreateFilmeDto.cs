1f20e209f78b1ebcf6070e55179252fb2b5a72ec
Henrique Mendes <henriquemendes13@gmail.com>
2024-10-13 21:07:07 -0300

    genero max 300 caracteres

--- FilmesApi/Data/Dtos/CreateFilmeDto.cs
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -10,6 +10,6 @@ public class CreateFilmeDto
     [StringLength(10, ErrorMessage = "O tamanho do gênero nao pode exceder 10 caracteres")]
     public string Genero { get; set; }
     [Required]
-    [Range(70, 300, ErrorMessage = "A duração deve ter entre 70 e 290 minutos")]
+    [Range(70, 300, ErrorMessage = "A duração deve ter entre 70 e 300 minutos")]
     public int Duracao { get; set; }
 }
